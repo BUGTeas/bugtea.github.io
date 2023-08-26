faf498cdb86689158c48af710222f226702925a0
BUGTeas <99161186+BUGTeas@users.noreply.github.com>
2023-08-26 19:20:48 +0800

    Add files via upload

--- 1919810/HHMLDocs/cn.js
+++ 1919810/HHMLDocs/cn.js
@@ -1,1 +1,105 @@
-
+loadInfo = "<h1>Hello HMCL! Launcher (HHML) 官方文档</h1>\
+<p><a href='itemDir/en.js'>English</a> <a href='itemDir/cn.js'>简体中文</a> <a href='https://github.com/BUGTeas/HHML/'>GitHub</a></p>\
+<p>文档适用版本：3.5.3.229 Update 1（最新）</p>\
+<h3>HHML 介绍</h3>\
+<p>Hello HMCL! Launcher（以下简称 HHML）是一个命令脚本（目前仅支持 Windows），从名称上看，它就是一个给启动器写的启动器，禁止套娃（</p>\
+<p>该脚本用于解决 HMCL 启动器（Hello Minecraft! Launcher）将相关配置文件和运行时放在系统用户目录下（Windows 下为“%userprofile%\\AppData\\Roaming.hmcl”），导致将其放在U盘中更换电脑后又要重新配置的问题。在 HMCL 启动前，脚本会将当前目录下的配置文件和已经下载的运行时还原到系统用户目录下，退出后再将其备份。</p>\
+<p>此外 HHML 脚本还能使得 HMCL 启动器检测当前目录下的Java并自动选择，无需安装或手动添加。</p>\
+<h2>简单用法</h2>\
+<ul style='list-style-type:decimal'>\
+    <li>下载你游玩的MC版本所需的 Java 的压缩包格式（不是安装程序，可以是 OpenJDK）并将其解压，然后从 HMCL 官方网站或 Github 上下载 JAR 格式 的 HMCL，再从本仓库的 Github Pages 下载最新版的 HHML 脚本：<br>Windows 中文版：<a href='/HHML/Windows/HHML-SC-GB2312.bat'>HHML-SC-GB2312.bat</a></br>Windows 英文版：<a href='/HHML/Windows/HHML-EN.bat'>HHML-EN.bat</a><br>如果你使用非简体中文版 Windows 或启用了 UTF-8 全球语言支持，请选择英文版，否则会出现乱码甚至报错！</li>\
+    <li>将脚本、解压出来的 Java 文件夹（可同时放置多个版本）和 HMCL 放到同一目录下（可以位于硬盘或U盘中）</li>\
+    <li>运行 HHML 脚本，脚本会自动搜索当前目录下的 Java 并挑选出最新版本来启动 JAR 格式的 HMCL，很快HMCL 的界面就会出现，但有时也会自动下载运行时（JavaFX），它将其放到了系统用户目录下，更换电脑后要重新下载，非常麻烦。所以 HHML 脚本会将其备份到当前目录下以备更换电脑时还原，节省了下载的时间。</li>\
+    <li>在 HHML 的帮助下，HMCL 不仅能自动搜索安装在系统中的 Java 还能自动搜索位于当前目录下的 Java。这意味着你不再需要手动为非安装版 Java 添加自定义路径（不像 PCL2，HMCL 只能添加一个自定义 Java 路径）</li>\
+    <li>此后，你应该使用 HHML 脚本来启动 HMCL，而不是直接打开 JAR 文件。为了提升启动速度，你可以在 HMCL 的高级设置中启用“不检查游戏完整性”选项，因为游戏启动前的完整性检查会将大量数据复制到系统磁盘，如果游戏在U盘上，这将耗费大量时间来完成此步骤。</li>\
+</ul>\
+<h2>高级用法</h2>\
+<h3>自定义 Java 路径（可使用相对路径）</h3>\
+<p>在脚本所在目录下创建一个名为“customPath.txt”的文本文档。内容是一行一个路径，就像这样：</p>\
+<textarea>\
+C:\\Users\\Administrator\\Desktop\\jdk\\archive\\openlogic-openjdk-8u372-b07-windows-64\n\
+.\\..\\..\\jdk\\jdk-11.0.12\n\
+\\software\\jdk\\archive\\jdk-17.0.8+7\
+</textarea>\
+<p>注意：Java 路径不是 Java 程序本体所在路径，而是“bin”文件夹所在位置，类似于 JAVA_HOME 变量。<p>\
+<h3>配置文件</h3>\
+<p>在脚本所在目录下创建一个名为“config.txt”的文本文档。里面不能有空行或注释，格式为“配置=参数”，就像这样：</p>\
+<textarea>\
+searchInCustPath=2\n\
+backupGlobalConfig=0\n\
+checkJava=0\n\
+use64java=1\
+</textarea>\
+<h3>自动备份/还原/删除配置</h3>\
+<p>注意：这里的运行时指的是 AuthLib-Injector（外置登录依赖）和 JavaFX（图形界面依赖），而全局配置指的是 config.json（用户许可）和 accounts.json（全局账户列表）。</p>\
+<p>如果你想在硬盘上使用 HHML，那你肯定不想它自动备份/还原/删除运行时和全局配置。它们都位于系统用户目录下，既然都在硬盘上，又不用换电脑，还有必要备份吗？</p>\
+<p>所以我提供了一些有关备份/还原/删除的选项（它们的默认参数都是1）：</p>\
+<p><b>还原运行时</b><br>配置名：restoreRunTime<br>参数：</p>\
+<textarea>\
+0: 从不还原\n\
+1: 还原 AuthLib（外置登录依赖），如果 Java 版本是 11 及以上且 HMCL 版本为 3.3.183 及以上再还原 JavaFX\n\
+2: 总是还原（无视 Java 和 HMCL 的版本）\
+</textarea>\
+<p><b>备份运行时</b><br>配置名：backupRunTime<br>参数：</p>\
+<textarea>\
+0: 从不备份\n\
+1: 当配置 restoreRunTime 启用且需要 HMCL 下载运行时的情况下全部备份\n\
+2: 总是备份（即使它们都已经备份）\
+</textarea>\
+<p><b>删除运行时</b><br>配置名：removeRunTime<br>参数：</p>\
+<textarea>\
+0: 从不删除\n\
+1: 当它们都被还原的情况下删除\n\
+2: 总是删除（即使它们都没有被还原）\
+</textarea>\
+<p><b>还原全局配置</b><br>配置名：restoreGlobalConfig<br>参数：</p>\
+<textarea>\
+0: 从不还原\n\
+1: 如果它们不存在于系统用户目录下则还原\n\
+2: 总是还原（即使它们本来就存在）\
+</textarea>\
+<p><b>备份全局配置</b><br>配置名：backupGlobalConfig<br>参数：</p>\
+<textarea>\
+0: 从不备份\n\
+1: 如果它们本来就存在则在 HMCL 关闭后弹窗让用户确认是否备份，否则自动备份\n\
+2: 总是备份（即使它们本来就存在）\
+</textarea>\
+<p><b>删除全局配置</b><br>配置名：removeGlobalConfig<br>参数：</p>\
+<textarea>\
+0: 从不删除\n\
+1: 如果它们已经还原则删除\n\
+2: 总是删除（即使它们本来就存在）\
+</textarea>\
+<h3>默认 Java 搜索范围配置</h3>\
+<p>你可以通过此选项设置脚本在某个范围的搜索,所有参数如下：</p>\
+<textarea>\
+0: 从不在此范围搜索\n\
+1: 如果上一范围中未找到 Java 则搜索此范围\n\
+2: 总是搜索此范围（即使 Java 已经找到）\
+</textarea>\
+<p><b>搜索自定义路径</b><br>配置名：searchInCustPath<br>默认参数：2<br>在此范围找到的 Java 会被添加到临时环境变量中，以便 HMCL 检测到。</p>\
+<p><b>搜索当前目录</b><br>配置名：searchInCD<br>默认参数：2<br>在此范围找到的 Java 会被添加到临时环境变量中，以便 HMCL 检测到。</p>\
+<p><b>搜索已安装应用列表</b><br>配置名：searchInInstApp<br>默认参数：1<br>注意：由于搜索此范围需要遍历大量注册表，因此速度较慢，所以默认只会在前面的范围中未找到 Java 的情况下搜索此范围。<br>如果你安装的 Java 不被 HMCL 检测到，可以将此配置的参数设为 2，在此范围找到的 Java 会被添加到临时环境变量中，以便 HMCL 检测到。</p>\
+<h3>默认 Java 检测和屏蔽配置</h3>\
+<p>你可以通过此选项设置脚本检测 Java 的规则：</p>\
+<p><b>检测 Java 版本</b><br>配置名：checkJava<br>默认参数：2<br>参数：</p>\
+<textarea>\
+0: 按文件顺序挑选最后一个 Java，不检测 Java 版本和位数（不推荐，需通过 use64java 参数手动指定 Java 的位数，在此模式下 Java 版本被拟定为 11）\n\
+1: 按文件顺序挑选最后一个 Java，并检测其版本和位数\n\
+2: 检测每个已找到的 Java 的版本和位数并挑选出最新版的 Java\
+</textarea>\
+<p><b>优先使用 Java 8（只在 checkJava 参数为 2 时生效）</b><br>配置名：useJava8First<br>默认参数：0<br>参数：</p>\
+<textarea>\
+0: 按版本顺序挑选最新版 Java\n\
+1: 按版本顺序优先挑选 Java 8 如果没有则挑选最新版 Java\
+</textarea>\
+<p><b>设置 Java 位数（只在 checkJava 参数为 0 时生效）</b><br>配置名：use64java<br>默认参数：x<br>参数：</p>\
+<textarea>\
+0: 32 位 Java（X86）\n\
+1: 64 位 Java（X64）\
+</textarea>\
+<p><b>屏蔽部分版本的 Java（只在 checkJava 参数为 2 时生效）</b><br>配置名：verBlackList<br>用法：将你不想使用的版本的版本号（可以多个,每个版本间用英符逗号隔开）写入参数中，在检测版本时会将与其对应的版本屏蔽掉。<br>默认参数：none<br>参数示例：</p>\
+<textarea>\
+verBlackList=1.8.0_272,17.0.1\
+</textarea>\
+"
---
+++ 1919810/HHMLDocs/en.js
@@ -0,0 +1,105 @@
+loadInfo = "<h1>Hello HMCL! Launcher (HHML) Official document</h1>\
+<p><a href='itemDir/en.js'>English</a> <a href='itemDir/cn.js'>简体中文</a> <a href='https://github.com/BUGTeas/HHML/'>GitHub</a></p>\
+<p>Document applicable version：3.5.3.229 Update 1（最新）</p>\
+<h3>HHML introduce</h3>\
+<p>Hello HMCL! Launcher（below referred to as HHML）is a command script (support Windows only now), look from the name, this is a launcher written for the launcher.</p>\
+<p>This script use to resolve the HMCL launcher (Hello Minecraft! Launcher) put some related config file and runtime to user directory in system, resulting in the need for reconfiguration on another system. The script will restore the downloaded runtime and global to user directory in system and back it up when exiting.</p>\
+<p>in addition, the HHML can also make HMCL detect and auto select Java under the current diredtory, needn't install or add manually</p>\
+<h2>Simple usage</h2>\
+<ul style='list-style-type:decimal'>\
+    <li>Download a Java archive (not the installer, can be OpenJDK) of the required version of the MC version you are playing and unzip it, and download latest version of HMCL in JAR format on the HMCL official webside, an download Latest HHML script from Github Pages：<br>Windows Chinese simplified editon: <a href='/HHML/Windows/HHML-SC-GB2312.bat'>HHML-SC-GB2312.bat</a></br>Windows English editon: <a href='/HHML/Windows/HHML-EN.bat'>HHML-EN.bat</a><br>if you using a non-Chinese simplified Windows or UTF-8 global language support has been enabled, please select the English edition, Otherwise garbled characters will appear, even cause errors!</li>\
+    <li>put the HMCL JAR file and HHML script and unzipped java archive (multiple different versions can be placed) in the same directory (can be on a flash drive or hard disk)</li>\
+    <li>run the HHML script, the script will secrch all Java archive in current directory automatic and pick the latest Java version to launch HMCL JAR file, soon the interface of HMCL will appear, but sometime also it's download runtimes (JavaFX) automatic, it puts it in the system by default, it is very troublesome to download it again after changing computers. So HHML can back it up to current directory in preparation for restoring to the system on another computer, saving time to download again.</li>\
+    <li>With the help of HHML script, the HMCL not only can search installed Java in the system automatic, but also can search unzipped Java in current directory automatic. This mains you don't need to manually add a non-installed Java version path to custom path (unlike PCL2, only one custom Java path can be added in HMCL)</li>\
+    <li>After that, HMCL should be launched via the HHML script instead of opening the JAR file directly. To speed up the game startup, you can enable \"Do not check game integrity\" option in advanced settings, because the intergrity check before the game launch requires a large amount of data to be copied to the system disk, if the game is on a flash disk, it will take a lot of time to complete this step.</li>\
+</ul>\
+<h2>Advenced usage</h2>\
+<h3>Custom Java path (relative path can be used)</h3>\
+<p>Create a text file under the same directory as the script and named \"customPath.txt\". The text content is one Java path per line, like this:</p>\
+<textarea>\
+C:\\Users\\Administrator\\Desktop\\jdk\\archive\\openlogic-openjdk-8u372-b07-windows-64\n\
+.\\..\\..\\jdk\\jdk-11.0.12\n\
+\\software\\jdk\\archive\\jdk-17.0.8+7\
+</textarea>\
+<p>note: The Java path is not the directory where the Java binary is located, but the directory where the \"bin\" folder is located, which is equivalent to the JAVA_HOME variable.<p>\
+<h3>Config file</h3>\
+<p>create a text file and named \"config.txt\". There can be no comments or blank lines in the file, and the format is \"configuration=parameter\", like this:</p>\
+<textarea>\
+searchInCustPath=2\n\
+backupGlobalConfig=0\n\
+checkJava=0\n\
+use64java=1\
+</textarea>\
+<h3>Auto backup/restore/remove configuration</h3>\
+<p>Note: runtime here refers to AuthLib-Injector (external login dependencies) and Java FX (graphic interface dependencies), while global config here refers to config.json (user license) and accounts.json (global user list).</p>\
+<p>auto backup/restore/remove the runtime and the globel configuration. They are all located at user directory in system, since it's all on the hard drive, and don't have to change computers, is it still need to backup?</p>\
+<p>So I provided some option about backup/restore/remove (they all parameter is 1): </p>\
+<p><b>Restore runtime</b><br>Configuration name: restoreRunTime<br>Parameter: </p>\
+<textarea>\
+0: Never restore\n\
+1: Restore AuthLib，restore JavaFX if Java version is 11 or later and HMCL version is 3.3.183 or later\n\
+2: Always restore authlib and JavaFX (ignore the Java and HMCL version)\
+</textarea>\
+<p><b>Backup runtime</b><br>Configuration name: backupRunTime<br>Parameter: </p>\
+<textarea>\
+0: Never backup\n\
+1: backup when \"restoreRunTime\" is enabled and need download runtime by HMCL. \n\
+2: Always backup (even if it has been backed up)\
+</textarea>\
+<p><b>Remove runtime</b><br>Configuration name: removeRunTime<br>Parameter: </p>\
+<textarea>\
+0: Never remove\n\
+1: Remove if they are both restored\n\
+2: Always remove (even if they are not restored)\
+</textarea>\
+<p><b>Restore global config</b><br>Configuration name: restoreGlobalConfig<br>Parameter: </p>\
+<textarea>\
+0: Never restore\n\
+1: Restore if they are both found in user directory in system\n\
+2: Always restore (even if it already exists before launch)\
+</textarea>\
+<p><b>Backup global config</b><br>Configuration name: backupGlobalConfig<br>Parameter: </p>\
+<textarea>\
+0: Never backup\n\
+1: If they already exist before launch, a pop-up window after the HMCL is closed allows the user to confirm whether to backup, otherwise the backup is automatically available\n\
+2: Always backup without user confirm  (even if it already exists before launch)\
+</textarea>\
+<p><b>Remove global config</b><br>Configuration name: removeGlobalConfig<br>Parameter: </p>\
+<textarea>\
+0: Never remove\n\
+1: Remove if it already restore\n\
+2: Always remove (even if it already exists before launch)\
+</textarea>\
+<h3>Default Java search range configuration</h3>\
+<p>You can set up script search in some range through this configuration, all parameter are follow:</p>\
+<textarea>\
+0: Never search in this range\n\
+1: Search this range if Java are not found at previous range\n\
+2: Always search (even if Java has been found)\
+</textarea>\
+<p><b>Search custom path</b><br>Configuration name: searchInCustPath<br>Default parameter: 2<br>Java found in this range will add to temporary environment variable so that HMCL to detect it.</p>\
+<p><b>Search current directory</b><br>Configuration name: searchInCD<br>Default parameter: 2<br>Java found in this range will add to temporary environment variable so that HMCL to detect it.</p>\
+<p><b>Search list of installed application</b><br>Configuration name: searchInInstApp<br>Default parameter: 1<br>Note: by default, search Java this range is only because it is not found at previous range, because it need traverse a lot of registry, it is too slow.<br>If you installed Java that can't be detected by HMCL, you can set the parameter of this configuration to 2, Java found in this range will add to temporary environment variable so that HMCL to detect it.</p>\
+<h3>Default Java detect & exclude configuration</h3>\
+<p>You can set the Java detection rules with this option:</p>\
+<p><b>Detect Java version</b><br>Configuration name: checkJava<br>Default parameter: 2<br>Parameter: </p>\
+<textarea>\
+0: Select last Java in the file order, not check Java version and bitness (not recommended, you need set bitness through \"use64Java\" configuration, the Java version will be proposed as 11)\n\
+1: Select last Java in the file order, and check its version and bitness\n\
+2: detect version and bitness of any founded Java and pick the latest version\
+</textarea>\
+<p><b>Use Java 8 preferred (only valid when parameter of checkJava is 2)</b><br>Default parameter: 0<br>Configuration name: useJava8First<br>Parameter: </p>\
+<textarea>\
+0: pick latest version Java by version order\n\
+1: pick Java 8 preferred by version order, pick latest version if its not found\
+</textarea>\
+<p><b>Set Java bitness (only valid when parameter of checkJava is 0)</b><br>Configuration name: use64java<br>Default parameter: x<br>Parameter: </p>\
+<textarea>\
+0: 32 Bit Java（X86）\n\
+1: 64 Bit Java（X64）\
+</textarea>\
+<p><b>Exclude some versions of Java (only valid when parameter of checkJava is 2)</b><br>Configuration name: verBlackList<br>Usage: enter unwanted version number in the parameter (can be multiple, use comma to separated between each version), corresponding version will be excluded during version detecting.<br>Default parameter: none<br>Parameter demo: </p>\
+<textarea>\
+verBlackList=1.8.0_272,17.0.1\
+</textarea>\
+"
