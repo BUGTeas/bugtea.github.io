474ddb2c0b0218b7953f9b7f3bb047ccbb05aef4
BUGTeas <99161186+BUGTeas@users.noreply.github.com>
2023-09-03 23:46:52 +0800

    Add files via upload

---
+++ 1919810/MCGroupRcmd/JsonList.js
@@ -0,0 +1,18 @@
+itemLoadJsonList = [,
+    {
+        "title":"基岩版原版生存<br>1.20.15.01",
+        "titlestyle":"background:rgba(0,0,0,0.6);padding:5px;border-radius:5px",
+        "cover":"itemDir/building.png",
+        "path":"bedrock"
+    },
+    {
+        "title":"大型中文整合包：<br>异界生活幻想",
+        "cover":"itemDir/clientShadowForest.png",
+        "path":"isekai"
+    },
+    {
+        "title":"梦回盘灵 ——<br>史上最大东方玄幻RPG地图：<br>盘灵古域 DLC",
+        "cover":"itemDir/emperorCity.jpg",
+        "path":"pangcont"
+    }
+]
